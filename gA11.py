22232d3a81d7a2967c17b2a18d63c12cafe4416b
Shrek <guaxinot@gmail.com>
2025-10-07 17:22:24 -0300

    Ronaldo

--- gA11.py
+++ gA11.py
@@ -7,6 +7,12 @@
 # git config --global user.name "Beltrano de Carvalho"
 # git config --global user.email "beltrano@ctrlplay.com.br"
 
-# git add ./arquivo
-# git commit -a -m "Mensagem do commit"
-# git push
+# git add ./arquivo | Prepara o arquivo para o commit
+# git commit -a -m "Mensagem do commit" | Realiza o commit das alterações
+# git push | Envia as alterações para o repositório remoto
+
+# git pull | Puxa as alterações do repositório remoto
+
+# git clone url | Cria uma copia ( clone ) do repositório do git hub
+
+# git status | Verifica o status do repositório
