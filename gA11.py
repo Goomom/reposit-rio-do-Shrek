8d8400dab413897f0943117b03619b9b6f3b8b2f
Shrek <guaxinot@gmail.com>
2025-10-07 17:11:38 -0300

    Ronaldo

--- gA11.py
+++ gA11.py
@@ -6,3 +6,7 @@
 # git init | Inicia o GIT no diretório escolhido
 # git config --global user.name "Beltrano de Carvalho"
 # git config --global user.email "beltrano@ctrlplay.com.br"
+
+# git add ./arquivo
+# git commit -a -m "Mensagem do commit"
+# git push
